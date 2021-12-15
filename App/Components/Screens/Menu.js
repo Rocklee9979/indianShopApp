93e82899864f33bcd93b9c16cfd5726d21a4ef7e
Pharsa Thapa <mail.pharsa.thapa@gmail.com>
2021-12-15 13:36:24 +1100

    removed unwanted files, fixed button styles bugs

--- App/Components/Screens/Menu.js
+++ App/Components/Screens/Menu.js
@@ -184,7 +184,7 @@ MenuItem:
 },
 MenuItemText:{
     fontSize: 18,
-    fontWeight: 300,
+    fontWeight: "300",
     color: '#2a368f',
     flex:0.7,
 },
@@ -196,7 +196,7 @@ subText:{
     fontSize:16,
     padding:10,
     color: '#2a368f',
-    fontWeight: 300,
+    fontWeight: "300",
 },
 seperator:{
     height: 0.5,
