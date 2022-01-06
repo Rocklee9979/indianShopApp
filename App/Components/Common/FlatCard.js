df5984788cff20ff978c6b8124576f3b95fe22d6
Neethu Majee <neethumajeesh92@gmail.com>
2022-01-06 14:45:04 +1100

    add price and brand name in product listing

--- App/Components/Common/FlatCard.js
+++ App/Components/Common/FlatCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 import RenderHTML from "react-native-render-html";
 import Icon from "react-native-vector-icons/FontAwesome";
 
@@ -36,9 +36,11 @@ export default function FlatCard({ product }) {
                  <Image source = { image_url } style = {styles.image}/>
                  <View style = {styles.contentContainer}>
                      <Title defaultPadding = '0' defaultColor = '#2a368f'> { product.name }</Title>
+                     <Text style = {styles.brandText}>Brand Name</Text>
                      <SubTitle>
                      { resultDesc }
                      </SubTitle>
+                     <Text style = {styles.priceText}>$ {product.price}</Text>
                      <View style = {styles.iconContainer}>
                      <Icon name='shopping-cart'  size={24} color='#2a368f' style = {{flex: 0.33, marginTop:15}}/>
                      <Icon name='heart'  size={24} color='#2a368f' style = {{flex: 0.33, marginTop:15}}/>
@@ -81,5 +83,19 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flex: 1,
         alignItems: 'center',
+    },
+    brandText: {
+        marginLeft: 10,
+        fontWeight: 'bold',
+        fontSize: 14,
+        color: '#f9db04',
+        fontStyle: 'italic',
+    },
+    priceText:{
+        marginLeft: 10,
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: '#2a368f',
+        fontStyle: 'italic',
     }
 })
